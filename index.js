c1337e94c6bc01c6d5ddc89c10607661a95de5bd
dillonpw <dillonpwalsh10@gmail.com>
2024-05-01 16:20:41 -0400

    initial commit job bpard scraper

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'JScraper' });
+});
+
+const server = app.listen(3000, () => {
+    console.log(`The application started on port ${server.address().port}`);
+});
