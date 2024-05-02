3d30814d5f4b44e694edd4f23325ee6bc281b7a2
dillonpw <dillonpwalsh10@gmail.com>
2024-05-02 09:14:14 -0400

    connected db install dotenv

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
 const express = require('express');
 const path = require('path');
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'JScraper' });
+    res.render('index', { title: 'full Stack' });
 });
 
 const server = app.listen(3000, () => {
     console.log(`The application started on port ${server.address().port}`);
 });
+
+const uri = process.env.DB_KEY
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db('admin').command({ ping: 1 });
+        console.log(
+            'Pinged your deployment. You successfully connected to MongoDB!'
+        );
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
